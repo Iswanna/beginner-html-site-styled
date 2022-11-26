aea43944ba3de676e0d05ecdaa017faca519bd25
Iswat <ibeetek@gmail.com>
2022-11-26 07:38:02 +0000

    Move folder to github

---
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -0,0 +1,11 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+/* solve a problem in that when you click on an image on your webpage, the image changes to another image
